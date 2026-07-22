8125266a5e632fbba6a5dff5018548607b80d1a8
Ciprian RUS <93035728+ciprian-rus@users.noreply.github.com>
2026-07-22 18:34:33 +0300

    feat(components): export navigation component contract

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -5,3 +5,8 @@ export {
   type ErrorSummaryFocusOptions,
   type FormComponentName,
 } from './forms.js';
+
+export {
+  navigationComponentNames,
+  type NavigationComponentName,
+} from './navigation.js';
